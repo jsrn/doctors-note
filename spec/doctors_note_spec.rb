37ecd85014d130ae6120c8be6d01bfb7e483a4d9
jsrn <james.srn@gmail.com>
2020-03-11 19:39:51 +0000

    No puts

--- spec/doctors_note_spec.rb
+++ spec/doctors_note_spec.rb
@@ -2,7 +2,6 @@ require './lib/doctors_note'
 
 RSpec.describe DoctorsNote, '#note' do
   it 'returns a hash' do
-    puts DoctorsNote.new.note
     expect(DoctorsNote.new.note).to be_a(Hash)
   end
 end
