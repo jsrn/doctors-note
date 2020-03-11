af0febf0a6ce681bb81f2a2bb7c32bcd976ebb7c
jsrn <james.srn@gmail.com>
2020-03-11 20:22:24 +0000

    validate fhir object

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'rspec'
+gem 'fhir_models'
--- spec/doctors_note_spec.rb
+++ spec/doctors_note_spec.rb
@@ -1,7 +1,14 @@
 require './lib/doctors_note'
+require 'fhir_models'
 
 RSpec.describe DoctorsNote, '#note' do
   it 'returns a hash' do
     expect(DoctorsNote.new.note).to be_a(Hash)
+  end
+
+  it 'represents a valid FHIR object' do
+    doctors_note = DoctorsNote.new.note
+    fhir_object = FHIR::Condition.new(doctors_note)
+    expect(fhir_object.valid?).to be true
   end
 end
